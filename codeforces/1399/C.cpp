ec4c1c7e251f2c78c3bab82113c268cb46ec7964
t1agogs <tiagofrauke@gmail.com>
2020-08-05 23:19:00 +0100

    Add solution for problem `C - Boats Competition` Link: https://codeforces.com/contest/1399/problem/C Tags: brute force, greedy, two pointers, *1200 Ref: https://codeforces.com/contest/1399/submission/89074401

--- codeforces/1399/C.cpp
+++ codeforces/1399/C.cpp
@@ -1,46 +1,42 @@
 #include <bits/stdc++.h>
 
-#define nl "\n"
-
-#define all(v) v.begin(),v.end()
-#define allr(v) v.rbegin(),v.rend()
-
-#define FOR(i,k,n) for(int i=k;i<n;i++)
-
-#define sz(vec) (int)vec.size()>
-
 using namespace std;
-
-void solve(int caseN)
-{
-    int N; cin>>N;
-    vector<int> a(N);
-    
-    for(auto &x:a) cin>>x;
-    
-    int ans=0;
-    for(int s=1;s<=2*N;s++)
-    {
-        int cnt[2*N+1]={0};
-        int pairs=0;
-        for(int i=0;i<N;i++)
-        {
-            if(s > a[i] && cnt[s-a[i]] > 0)
-            {
-                pairs++;
-                cnt[s-a[i]]--;
-            }
-            else cnt[a[i]]++;
-        }
-        ans=max(ans,pairs);
-    }
-    cout<<ans<<nl;
-}
 
 int main()
 {
-    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-	int T; cin>>T;
-	while(T--)
-	    solve(T+1);
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	
+	int t,n; cin>>t;
+	
+	while(t--)
+	{
+	    cin>>n;
+	    vector<int> vec;
+	    int ans=0;
+	    
+	    for(int i=0;i<n;i++)
+	    {
+	        int a;
+	        cin>>a;
+	        vec.push_back(a);
+	    }
+	    
+	    for(int s=2;s<=2*n;s++)
+	    {
+	        int counts[n*2+1]={0},pairs=0;
+	        for(int act=0;act<(int)vec.size();act++)
+	        {
+	            if(s > vec[act] && counts[s-vec[act]] != 0)
+	            {
+	                counts[s-vec[act]]--;
+	                pairs++;
+	            }
+	            else counts[vec[act]]++;
+	        }
+	        ans=max(ans,pairs);
+	    }
+	    
+	    cout<<ans<<"\n";
+	    
+	}
 }

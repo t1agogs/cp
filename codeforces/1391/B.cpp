2a2831ec683b34a92b24adb354002b926a1579e0
t1agogs <tiagofrauke@gmail.com>
2020-08-09 18:53:00 +0100

    Add solution for problem `B - Fix You` Link: https://codeforces.com/contest/1391/problem/B Tags: brute force, greedy, implementation, *800 Ref: https://codeforces.com/contest/1391/submission/89458296

--- codeforces/1391/B.cpp
+++ codeforces/1391/B.cpp
@@ -2,35 +2,28 @@
 
 #define nl "\n"
 
-#define all(v) v.begin(),v.end()
-#define allr(v) v.rbegin(),v.rend()
-
-#define FOR(i,k,n) for(int i=k;i<n;i++)
-
-#define sz(vec) (int)vec.size()>
-
 using namespace std;
-
-void solve(int caseN)
-{
-    int N,M; cin>>N>>M;
-    vector<vector<char>> a(N,vector<char>(M));
-    
-    for(int i=0;i<N;i++)
-        for(int j=0;j<M;j++)
-            cin>>a[i][j];
-    
-    int sum=0;
-    for(int i=0;i<M;i++) sum+=a[N-1][i] == 'D' ?1:0;
-    for(int i=0;i<N;i++) sum+=a[i][M-1] == 'R' ?1:0;
-    
-    cout<<sum<<nl;
-}
 
 int main()
 {
-    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	
 	int T; cin>>T;
+	
 	while(T--)
-	    solve(T+1);
+	{
+	    int n,m,total=0; cin>>n>>m;
+	    
+	    for(int i=0;i<n;i++)
+	    {
+	        for(int j=0;j<m;j++)
+	        {
+	            char s; cin>>s;
+	            if(i+1==n && s!='R') total++;
+	            else if(j+1==m && s!='D') total++;
+	        }
+	    }
+	    
+	    cout<<total-1<<nl;
+	}
 }

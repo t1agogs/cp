55cfabfe2b39d90ea3e97e439e6d31c4673cc8b8
t1agogs <tiagofrauke@gmail.com>
2020-09-08 01:35:00 +0100

    Add solution for problem `A - Permutation Forgery` Link: https://codeforces.com/contest/1405/problem/A Tags: constructive algorithms, *800 Ref: https://codeforces.com/contest/1405/submission/92173716

---
+++ codeforces/1405/A.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#define nl "\n"
+
+#define all(v) v.begin(),v.end()
+#define allr(v) v.rbegin(),v.rend()
+
+#define FOR(i,k,n) for(int i=k;i<n;i++)
+
+#define sz(vec) (int)vec.size()>
+
+using namespace std;
+
+void solve(int caseN)
+{
+    int N; cin>>N;
+    vector<int> p(N);
+    
+    for(auto &s: p) cin>>s;
+    
+    for(int i=N-1;i>=0;i--) cout<<p[i]<<" ";
+    cout<<nl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+	int T; cin>>T;
+	while(T--)
+	    solve(T+1);
+}

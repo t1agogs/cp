1cc0a6148abf662f588d7152997f885da9cefc90
t1agogs <tiagofrauke@gmail.com>
2020-09-10 12:02:00 +0100

    Add solution for problem `B - CopyCopyCopyCopyCopy` Link: https://codeforces.com/contest/1325/problem/B Tags: greedy, implementation, *800 Ref: https://codeforces.com/contest/1325/submission/92419730

---
+++ codeforces/1325/B.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#define nl "\n"
+
+#define all(v) v.begin(),v.end()
+#define allr(v) v.rbegin(),v.rend()
+
+#define FOR(i,k,n) for(int i=k;i<n;i++)
+
+#define sz(vec) (int)vec.size()>
+
+using namespace std;
+
+void solve(int caseN)
+{
+    int N; cin>>N;
+    set<int> s;
+    
+    for(int i=0;i<N;i++)
+    {
+        int x; cin>>x;
+        s.insert(x);
+    }
+    cout<<s.size()<<nl;
+    
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+	int T; cin>>T;
+	while(T--)
+	    solve(T+1);
+}

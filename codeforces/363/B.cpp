33894c5f01d97726d247afb5f0eeee1c558a39a1
t1agogs <tiagofrauke@gmail.com>
2020-10-03 22:30:00 +0100

    Add solution for problem `B - Fence` Link: https://codeforces.com/contest/363/problem/B Tags: brute force, dp, *1100 Ref: https://codeforces.com/contest/363/submission/94601067

--- codeforces/363/B.cpp
+++ codeforces/363/B.cpp
@@ -1,51 +1,34 @@
 #include <bits/stdc++.h>
- 
-#define nl "\n"
-#define ll long long
-#define vii(N,M,x) vector<vector<long long>> x(N,vector<long long>(M))
 
-#define all(v) v.begin(),v.end()
-#define allr(v) v.rbegin(),v.rend()
- 
-#define FOR(i,k,n) for(int i=k;i<n;i++)
- 
-#define sz(vec) (int)vec.size()>
- 
+#define nl "\n"
+#define all(v) v.begin(), v.end()
+
 using namespace std;
 
-void solve(/*int caseN*/)
+int MOD=1e9+7;
+
+int main()
 {
-	int N,k; cin>>N>>k;
+	int N,K; cin>>N>>K;
 	vector<int> a(N);
 	
 	for(auto &x:a) cin>>x;
 	
-	int total=0,curr=0;
-	int index=-1;
-	for(int i=0;i<N;i++)
+	vector<long long> pre_sum(N+1);
+	
+	for(int i=1;i<=N;i++)
+		pre_sum[i]=pre_sum[i-1]+a[i-1];
+		
+	long long minN=1e18;
+	int ind=0;
+	for(int i=1;i<=N;i++)
 	{
-		curr+=a[i];
-		if(i>k-1)
+		if(minN > (i-K<0?1e18:pre_sum[i]-pre_sum[i-K]))
 		{
-			curr-=a[i-k];
-			if(curr < total)
-				total=curr,index=i;
-		}
-		else if(i==k-1)
-		{
-			index=i;
-			total=curr;
+			minN=pre_sum[i]-pre_sum[i-K];
+			ind=i;
 		}
 	}
 	
-	cout<<(index-(k-1))+1;
-}
- 
-int main()
-{
-    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-	
-	//int T; cin>>T;
-	//while(T--)
-	    solve(/*T*/);
+	cout<<ind-K+1;
 }

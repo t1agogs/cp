3901d9e6feb28fed71b63cc0c482aa5617de588e
t1agogs <tiagofrauke@gmail.com>
2020-10-04 14:28:00 +0100

    Add solution for problem `B - Lovely Palindromes` Link: https://codeforces.com/contest/688/problem/B Tags: constructive algorithms, math, *1000 Ref: https://codeforces.com/contest/688/submission/94649180

---
+++ codeforces/688/B.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#define nl "\n"
+#define all(v) v.begin(), v.end()
+
+using namespace std;
+
+int MOD=1e9+7;
+
+int main()
+{
+	string s; cin>>s;
+	cout<<s;
+	reverse(s.begin(),s.end());
+	cout<<s;
+}
